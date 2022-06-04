c2610e5c9a312580a4eb28f6f82c5ba49c01e23c
Anton Dobrynin <andeedobro@gmail.com>
2022-06-04 23:41:09 +0500

    cors

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, {
-        cors: true
-    });
+    const app = await NestFactory.create(AppModule);
+
+    app.enableCors({
+        origin: "*",
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        preflightContinue: false,
+        optionsSuccessStatus: 204
+    })
 
     app.useGlobalPipes(
         // Пайпа валидации дтошек
